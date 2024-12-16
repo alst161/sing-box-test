f3fc3b9eb5e429d04fca31bec57c99c5afb4c404
alst161 <alst161@gmail.com>
2024-12-17 01:46:39 +0200

    vision

--- protocol/vless/service.go
+++ protocol/vless/service.go
@@ -3,7 +3,6 @@ package vless
 import (
 	"context"
 	"encoding/binary"
-	"github.com/sagernet/sing-vmess/vless"
 	"io"
 	"net"
 
@@ -54,7 +53,7 @@ func (s *Service[T]) UpdateUsers(userList []T, userUUIDList []string, userFlowLi
 }
 
 func (s *Service[T]) NewConnection(ctx context.Context, conn net.Conn, source M.Socksaddr, onClose N.CloseHandlerFunc) error {
-	request, err := vless.ReadRequest(conn)
+	request, err := ReadRequest(conn)
 	if err != nil {
 		return err
 	}
@@ -64,8 +63,8 @@ func (s *Service[T]) NewConnection(ctx context.Context, conn net.Conn, source M.
 	}
 	ctx = auth.ContextWithUser(ctx, user.String())
 	userFlow := request.Flow
-	if request.Flow == vless.FlowVision && request.Command == vmess.NetworkUDP {
-		return E.New(vless.FlowVision, " flow does not support UDP")
+	if request.Flow == FlowVision && request.Command == vmess.NetworkUDP {
+		return E.New(FlowVision, " flow does not support UDP")
 	} else if request.Flow != userFlow {
 		return E.New("flow mismatch: expected ", flowName(userFlow), ", but got ", flowName(request.Flow))
 	}
@@ -76,8 +75,8 @@ func (s *Service[T]) NewConnection(ctx context.Context, conn net.Conn, source M.
 	}
 	responseConn := &serverConn{ExtendedConn: bufio.NewExtendedConn(conn), writer: bufio.NewVectorisedWriter(conn)}
 	switch userFlow {
-	case vless.FlowVision:
-		conn, err = vless.NewVisionConn(responseConn, conn, request.UUID, s.logger)
+	case FlowVision:
+		conn, err = NewVisionConn(responseConn, conn, request.UUID, s.logger)
 		if err != nil {
 			return E.Cause(err, "initialize vision")
 		}
@@ -118,7 +117,7 @@ func (c *serverConn) Read(b []byte) (n int, err error) {
 
 func (c *serverConn) Write(b []byte) (n int, err error) {
 	if !c.responseWritten {
-		_, err = bufio.WriteVectorised(c.writer, [][]byte{{vless.Version, 0}, b})
+		_, err = bufio.WriteVectorised(c.writer, [][]byte{{Version, 0}, b})
 		if err == nil {
 			n = len(b)
 		}
@@ -131,7 +130,7 @@ func (c *serverConn) Write(b []byte) (n int, err error) {
 func (c *serverConn) WriteBuffer(buffer *buf.Buffer) error {
 	if !c.responseWritten {
 		header := buffer.ExtendHeader(2)
-		header[0] = vless.Version
+		header[0] = Version
 		header[1] = 0
 		c.responseWritten = true
 	}
@@ -140,7 +139,7 @@ func (c *serverConn) WriteBuffer(buffer *buf.Buffer) error {
 
 func (c *serverConn) WriteVectorised(buffers []*buf.Buffer) error {
 	if !c.responseWritten {
-		err := c.writer.WriteVectorised(append([]*buf.Buffer{buf.As([]byte{vless.Version, 0})}, buffers...))
+		err := c.writer.WriteVectorised(append([]*buf.Buffer{buf.As([]byte{Version, 0})}, buffers...))
 		c.responseWritten = true
 		return err
 	}
@@ -195,14 +194,14 @@ func (c *serverPacketConn) WriteTo(p []byte, addr net.Addr) (n int, err error) {
 		if c.responseWriter == nil {
 			var packetLen [2]byte
 			binary.BigEndian.PutUint16(packetLen[:], uint16(len(p)))
-			_, err = bufio.WriteVectorised(bufio.NewVectorisedWriter(c.ExtendedConn), [][]byte{{vless.Version, 0}, packetLen[:], p})
+			_, err = bufio.WriteVectorised(bufio.NewVectorisedWriter(c.ExtendedConn), [][]byte{{Version, 0}, packetLen[:], p})
 			if err == nil {
 				n = len(p)
 			}
 			c.responseWritten = true
 			return
 		} else {
-			_, err = c.responseWriter.Write([]byte{vless.Version, 0})
+			_, err = c.responseWriter.Write([]byte{Version, 0})
 			if err != nil {
 				return
 			}
@@ -233,11 +232,11 @@ func (c *serverPacketConn) WritePacket(buffer *buf.Buffer, destination M.Socksad
 		if c.responseWriter == nil {
 			var packetLen [2]byte
 			binary.BigEndian.PutUint16(packetLen[:], uint16(buffer.Len()))
-			err := bufio.NewVectorisedWriter(c.ExtendedConn).WriteVectorised([]*buf.Buffer{buf.As([]byte{vless.Version, 0}), buf.As(packetLen[:]), buffer})
+			err := bufio.NewVectorisedWriter(c.ExtendedConn).WriteVectorised([]*buf.Buffer{buf.As([]byte{Version, 0}), buf.As(packetLen[:]), buffer})
 			c.responseWritten = true
 			return err
 		} else {
-			_, err := c.responseWriter.Write([]byte{vless.Version, 0})
+			_, err := c.responseWriter.Write([]byte{Version, 0})
 			if err != nil {
 				return err
 			}
---
+++ protocol/vless/vision_reality.go
@@ -0,0 +1,22 @@
+//go:build with_reality_server
+
+package vless
+
+import (
+	"net"
+	"reflect"
+	"unsafe"
+
+	"github.com/sagernet/sing/common"
+	"github.com/xtls/reality"
+)
+
+func init() {
+	tlsRegistry = append(tlsRegistry, func(conn net.Conn) (loaded bool, netConn net.Conn, reflectType reflect.Type, reflectPointer uintptr) {
+		tlsConn, loaded := common.Cast[*reality.Conn](conn)
+		if !loaded {
+			return
+		}
+		return true, tlsConn.NetConn(), reflect.TypeOf(tlsConn).Elem(), uintptr(unsafe.Pointer(tlsConn))
+	})
+}
